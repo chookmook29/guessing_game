6ba02c1036940602077c6776f94e19be3a88f58e
Piotr Chudziak <strangelove44@gmail.com>
2018-11-01 21:22:22 +0000

    User add form and display function

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return render_template("index.html")
+    return render_template("index.html", user = "")
+
+@app.route("/", methods = ["POST", "GET"])
+def user_display():
+    return render_template("index.html", user = "Welcome " + request.form["new_user"] + "!")
