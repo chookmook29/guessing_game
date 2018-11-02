2690488f40773c33f82eb6d8056022efddfc2831
Piotr Chudziak <strangelove44@gmail.com>
2018-11-02 13:08:05 +0000

    Basic implementation of session extension

--- run.py
+++ run.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import random
+
 app = Flask(__name__)
+
+app.secret_key = 'secret'
 
 @app.route("/")
 def index():
-    return render_template("index.html", user = "")
+	return render_template("index.html", user = "")
 
 @app.route("/", methods = ["POST", "GET"])
 def user_display():
-    return render_template("game.html", user = "Welcome " + request.form["new_user"] + "!")
+	array = ("_ _ _", "_ _ _")
+	current = random.choice(array)
+	session["current"] = current
+	user = request.form["new_user"]
+	user_greeting = "Welcome " + request.form["new_user"] + "!"
+	session["user_greeting"] = user_greeting
+	return render_template("game.html", user_greeting = user_greeting, current = current)
+
+@app.route("/answer/", methods=['POST', "GET"])
+def answer():
+	answer = request.form["answer"]
+	return render_template("game.html", answer = answer, current = session.get("current"), user_greeting = session.get("user_greeting"))
+	
