c64f3efd2f9f8fddfe159e19eee6d8783d3a4994
Piotr Chudziak <strangelove44@gmail.com>
2018-11-30 21:05:41 +0000

    Add tests folder with one unittest example

---
+++ tests/test_run.py
@@ -0,0 +1,14 @@
+import unittest
+import random
+
+class test_if_statement(unittest.TestCase):
+	def test_statements(self):
+		array = ("ALLIGATOR", "BADGER", "BEAR", "BISON", "DEER", "DODO", "FOX", "LYNX", "PORCUPINE", "RHINOCEROS", "WOLF")
+		letters = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+		current = random.choice(array)
+		guess = random.choice(letters)
+		self.assertIn(guess, current)
+		
+
+if __name__ == '__main__':
+    unittest.main()
