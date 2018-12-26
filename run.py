5ab2bb7624d130aaa34ea00882ede42a09751786
Piotr Chudziak <strangelove44@gmail.com>
2018-12-26 23:52:40 +0000

    Add function that helps storing user data

--- run.py
+++ run.py
@@ -130,6 +130,19 @@ def check():
 		session["highscore"] = highscore
 		return render_template("score.html", highscore = highscore, user = user, score = score)
 
+@app.route("/back", methods=['POST', "GET"])
+def back():
+	letter_array = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+	current_hidden = session.get("current_hidden")
+	current = session.get("current")
+	current_image = session.get("current_image")
+	user = session.get("user")
+	score = session.get("score")
+	attempts =  session.get("attempts")
+	used = session.get("used")
+	guess = " "
+	return render_template("game.html", guess = guess, current = current_hidden, current_image = current_image, user_greeting = "TRY NOW!", used = used, score = score, user = user, letter_array = letter_array,  attempts = attempts)
+
 @app.route("/rules")
 def rules():
 	user = session.get("user")
